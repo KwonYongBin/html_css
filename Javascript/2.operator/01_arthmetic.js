d8b43f0a2ab6b38795ab7e79e0a933af3b4827f7
KwonYongBin <dydqls6789@naver.com>
2025-08-19 16:58:56 +0900

    2025.08.19

---
+++ Javascript/2.operator/01_arthmetic.js
@@ -0,0 +1,20 @@
+/**
+ * 사칙연산 : +, -, *, /, %, **
+ */
+console.log(3 + 5); // 8
+console.log(3 - 5); // -2
+console.log(3 * 5); // 15
+console.log(3 / 5); // 0.6
+console.log(3 % 5); // 3
+console.log(3 ** 5); // 243
+
+// 짝수, 홀수, 선택 : %
+let a = 101; // 홀수
+a = 100; //짝수
+console.log((a % 2) ? "홀수" : "짝수");
+
+//접합 연산자 : +
+console.log(10 + 20); // 30
+console.log('10' + 20); // 1020
+console.log('합계 = ' + a);
+console.log(`합계 = ${a}`);
---
+++ Javascript/2.operator/04_realational.js
@@ -0,0 +1,23 @@
+/**
+ * 비교 연산자 >, <, >=, <=, ==(값 비교), ===(값,데이터타입)
+ */
+console.log(3 > 2);       //true
+console.log(3 < 2);       //flase
+console.log(3 >= 2);      //true
+console.log(3 <= 2);      //false
+console.log(3 == '3');    //true
+console.log(3 === '3');   //false
+console.log(3 == 'A');    //false A = 67(아스키 문자와 비교) 3 == 67
+console.log(3 === 'a');   //false
+
+let obj1 = {
+    name: '홍길동'
+}
+let obj2 = {
+    name: '홍길동'
+}
+
+console.log("참조형-------------------");
+console.log(obj1 == obj2); //false - 주소값이 다르기 때문에
+console.log(obj1.name == obj2.name);
+console.log(typeof obj1 === typeof obj2);
