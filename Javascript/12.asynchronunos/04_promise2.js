31667d3e1ebfffbfb06199a4138190a0191bcc68
KwonYongBin <dydqls6789@naver.com>
2025-09-03 16:21:13 +0900

    2025.09.03

--- Javascript/12.asynchronunos/04_promise2.js
+++ Javascript/12.asynchronunos/04_promise2.js
@@ -25,5 +25,31 @@ async function pizzaOrder (){
     console.log(`3. 배달 완료`);
 }
 
+pizzaOrder();
+
+
+/**
+ * Pizza 주문 처리 로직
+ * - 1. 주문
+ * - 2. 피자만들기 (2.5초 소요)
+ * - 3. 배달
+ */
+
+async function pizzaOrder () {
+    console.log("1. 주문 완료");
+
+    try {
+        await new Promise((resolve) => {
+            setTimeout(() => {
+                console.log("2. 피자완성");
+                resolve();
+            }, 2500);
+        });
+
+        console.log("3. 배달 완료");
+    } catch (err) {
+        console.log("🚨 주문 처리 중 오류 발생:", err);
+    }
+}
 
 pizzaOrder();
---
+++ Javascript/json/01_key.js
@@ -0,0 +1,4 @@
+//kobis key = 1387ed83604df30a0c5d9dfdea0cba00
+
+//키값 반영 주소
+//https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=1387ed83604df30a0c5d9dfdea0cba00&targetDt=20120101
